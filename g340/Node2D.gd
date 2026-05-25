dd89807b8877e33d1b1e64679764ed9b16b14022
minec-c <reciprocalidea@gmail.com>
2026-05-24 22:45:06 -0300

    Update print statement in _ready function

--- g340/Node2D.gd
+++ g340/Node2D.gd
@@ -8,7 +8,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print("test 2105261817")
+	print("test 2405262243")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
